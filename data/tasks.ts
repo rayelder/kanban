07bf1d9bdc7081b1d5af31126e6aba8cd9f6f57c
Ray Elder <hello@rayelder.com>
2024-06-30 19:38:30 -0700

    Create data store for tasks

---
+++ data/tasks.ts
@@ -0,0 +1,22 @@
+const tasks = [
+    {
+        uniqueID: '588a7febcb222',
+        columnID: 0,
+        show: true,
+        title: 'Title 0',
+    },
+    {
+        uniqueID: 'b10a9011492b7',
+        columnID: 0,
+        show: true,
+        title: 'Title 1',
+    },
+    {
+        uniqueID: 'e4e7081acf261',
+        columnID: 1,
+        show: true,
+        title: 'Title 2',
+    },
+]
+
+export default tasks
